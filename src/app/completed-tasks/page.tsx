aefc56ef211f005a391a5dea36f05a2cd62078a0
Qizhy-DEV <dashlyuidesigns@gmail.com>
2025-05-06 12:41:40 +0700

    resolve prettier

--- src/app/completed-tasks/page.tsx
+++ src/app/completed-tasks/page.tsx
@@ -1,17 +1,18 @@
-'use client'
-import '@/styles/all-tasks.css'
-import FilterLayout from '@/components/filter-layout'
-import HeaderLayout from '@/components/header-layout'
-import TasksLayout from '@/components/tasks-layout'
-import React from 'react'
+'use client';
+import '@/styles/all-tasks.css';
+import FilterLayout from '@/components/filter-layout';
+import HeaderLayout from '@/components/header-layout';
+import TasksLayout from '@/components/tasks-layout';
+import React from 'react';
 
 const CompletedTasksPage = () => {
-  
-    return <section className='content'>
-        <HeaderLayout/>
-        <FilterLayout/>
-        <TasksLayout/>
+  return (
+    <section className="content">
+      <HeaderLayout />
+      <FilterLayout />
+      <TasksLayout />
     </section>
-}
+  );
+};
 
-export default CompletedTasksPage
+export default CompletedTasksPage;
