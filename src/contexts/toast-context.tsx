47504ac9ee9492f4c9c3281892d27fde36ea7b1e
Qizhy-DEV <dashlyuidesigns@gmail.com>
2025-05-07 10:18:14 +0700

    Implement Toast and Navbar

--- src/contexts/toast-context.tsx
+++ src/contexts/toast-context.tsx
@@ -11,9 +11,6 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
   const [toasts, setToasts] = useState<Toast[]>([]);
 
   const notify = (toast: Omit<Toast, 'id'>) => {
-    console.log('call');
-
-    // const newId = uuidv4();
 
     const { done, status, subtitle, title, duration } = toast;
 
@@ -26,23 +23,10 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
       duration,
     };
 
-    console.log(toasts);
-
     setToasts((prev) => {
-      console.log(prev);
       return [...prev, newToast];
     });
   };
-
-  useEffect(() => {
-    // if (toasts.length > 0) {
-    //   const waitingElement = toasts.find(item => item.done === false)
-    //   if (!waitingElement) {
-    //     setToasts([])
-    //   }
-    // }
-    console.log(toasts);
-  }, [toasts]);
 
   const clear = (id: string) => {
     setToasts((prev) => prev.filter((item) => item.id !== id));
@@ -51,8 +35,8 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ToastContext.Provider value={{ notify }}>
       <div id="toast">
-        {toasts.map((toast, index) => (
-          <ToastElement toast={toast} clear={clear} key={index} />
+        {toasts.map((toast) => (
+          <ToastElement toast={toast} clear={clear} key={toast.id} />
         ))}
       </div>
       {children}
