02cb094f5ac9e9d74b7ae8e34e92b0438d8b6e0b
Qizhy-DEV <dashlyuidesigns@gmail.com>
2025-05-07 13:49:12 +0700

    Implement Toast and Navbar

--- src/components/error-text.tsx
+++
@@ -1,8 +0,0 @@
-import { FieldError } from 'react-hook-form';
-
-type Props = {
-  error?: FieldError;
-};
-
-export const ErrorText = ({ error }: Props) =>
-  error?.message ? <p style={{ color: 'red' }}>{error.message}</p> : null;
